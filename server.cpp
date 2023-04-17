60901246350aee20ec06515aa753cdb7ccaf1a4e
AnvarKilyb <anvar.kily@gmail.com>
2023-04-17 18:27:02 +0200

    TCP and UDP implementation + bug fixes

--- server.cpp
+++ server.cpp
@@ -8,6 +8,8 @@
 #include <cpuid.h>
 #include <string>
 #include <sstream>
+#include <regex>
+#include <csignal>
 
 #define BUFFERSIZE 512
 
@@ -21,18 +23,26 @@
 #define TEXT_WRONG_FORMAT -3
 
 using namespace std;
+char buffer[BUFFERSIZE];
 char *HOST, *MODE;
 int PORT = 0;
+int sockfd, newsockfd, pid;
 short connections = 0;
 
-// Receiveng messages from client, then sending results
+// Receiveng messages from client, then sending results, only for TCP
 void event_handler(int);
 // Helping function to write messages to client back
 void send_message(int, string);
-// Function to compute SOLVE command
-double text_solve(const char*, short*);
+// Function to check command "SOLVE" and compute lisp expression
+double text_solve(string, short*);
 // Basic function to print ERRORs
 void print_err(string);
+// Function to compute lisp expression
+int expr(const smatch&);
+// Function to parse lisp expression
+int lisp(string, bool*);
+
+regex LISP_RE(R"(\(([+\-*/]) (\d+) (\d+)\))");
 
 // Basic function to parse arguments
 void check_args(int argc, char *argv[]){
@@ -62,20 +72,49 @@ void check_args(int argc, char *argv[]){
         print_err("Invalid mode. Enter either tcp or udp");
 }
 
+//Signal handler for SIGINT
+void signalHandler(int signum){
+    // Only parent message needs to print message
+    if(pid > 0) cout << "Interrupt signal (" << signum << ") received\n";
+
+    //check if buffer is empty
+    if (strcmp(buffer, "") != 0) {
+        memset(buffer, 0, BUFFERSIZE);
+    }
+
+    //Pressing ctrl+c and using TCP sends BYE to client
+    if (strcmp(MODE, "tcp") == 0) {
+        // Before interrupting sends BYE message to client
+        if(pid == 0){
+            close(sockfd);
+            send_message(newsockfd, "BYE\n");
+        }else{
+            close(newsockfd);
+            
+        }
+    }
+
+    //close sockets
+    if(newsockfd != 0) close(newsockfd);
+    if(sockfd != 0) close(sockfd);
+    fflush(stdout);
+
+    exit(signum);
+
+}
+
 // Run server if TCP connection
 void run_tcp(){
-    printf("TCP running...\n");
-    int sockfd, newsockfd, portno, pid;
-    char buffer[BUFFERSIZE];
+    cout << "TCP running..." << endl;
     struct sockaddr_in serv_addr, cli_addr;
     // Creating socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if(sockfd < 0) print_err("Error opening socket");
     bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = PORT;
+    // Filling server information
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(HOST);
-    serv_addr.sin_port = htons(portno);
+    serv_addr.sin_port = htons(PORT);
     if (bind(sockfd, (struct sockaddr *) &serv_addr,
               sizeof(serv_addr)) < 0) {
                 print_err("ERROR on binding");
@@ -105,11 +144,69 @@ void run_tcp(){
 }
 // Run server if UDP connection
 void run_udp(){
-    printf("UDP running...\n");
+    cout << "UDP running..." << endl;
+    struct sockaddr_in servaddr, cliaddr;
+
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) print_err("ERROR opening socket");
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
+    // Filling server information
+    servaddr.sin_family    = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr(HOST);
+    servaddr.sin_port = htons(PORT);
+    // Bind the socket with the server address
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, 
+            sizeof(servaddr)) < 0 )
+    {
+        print_err("ERROR on binding");
+    }
+    while(true){
+        memset(buffer, 0, BUFFERSIZE);
+        socklen_t len;
+        int n;
+        bool ok_flag = true;
+
+        len = sizeof(cliaddr);
+        n = recvfrom(sockfd, (char *)buffer, BUFFERSIZE, 
+                    MSG_WAITALL, ( struct sockaddr *) &cliaddr,
+                    &len);
+        buffer[n] = '\0';
+        cout << "Received datagram from client: " << buffer + 2;
+
+        // Result data that will be sent back to client
+        char result_data[BUFFERSIZE];
+        bzero(result_data, BUFFERSIZE);
+
+        int expr_result = lisp(buffer + 2, &ok_flag);
+        char prep_data[BUFFERSIZE];
+        bzero(prep_data, BUFFERSIZE);
+        result_data[0] = 1;
+        if(ok_flag){
+            strncpy(prep_data, to_string(expr_result).c_str(), sizeof(prep_data));
+            result_data[1] = '\0'; //OK status
+            result_data[2] = static_cast<char>(strlen(prep_data));
+            memcpy(&result_data[3], prep_data, strlen(prep_data));
+            cout << "\nResult: " << result_data + 3 << endl;
+            sendto(sockfd, result_data, strlen(prep_data) + 3, MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
+            cout << "Result data sent" << endl << endl;
+        }
+        else{
+            result_data[1] = 1; //Error status
+            result_data[2] = strlen(prep_data);
+            strcat(prep_data, "Invalid argument\n");
+            memcpy(result_data + 3, prep_data, strlen(prep_data));
+            cout << "\nInvalid lisp expression, sending to client wrong data" << endl << endl;
+            sendto(sockfd, result_data, strlen(prep_data) + 3, MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
+        }
+    }
 }
 
-
 int main(int argc, char *argv[]){
+    //Register signal handler
+    signal(SIGINT, signalHandler);
+
     check_args(argc, argv);
 
     if(strcmp(MODE, "tcp") == 0){
@@ -181,10 +278,15 @@ void event_handler(int sock){
                 cout << connections << ":"  << "Sending result message to client" << endl << endl;
                 send_message(sock, prep_message);
             }else{
-                cout << connections << ":"  << "Received wrong command from client, interrupting connection..." << buffer << endl << endl;
+                cout << connections << ":"  << "Received wrong command from client, interrupting connection..." << endl << endl;
                 send_message(sock, "BYE\n");
                 break;
             }
+        }
+        else{
+            cout << connections << ":"  << "Received wrong command from client, interrupting connection..." << endl << endl;
+            send_message(sock, "BYE\n");
+            break;
         }
     }
 
@@ -195,45 +297,53 @@ void send_message(int sock, string msg){
         print_err("ERROR writing to socket");
 }
 
-// Function to compute SOLVE command
-double text_solve(const char* input, short* err_code){
-    stringstream ss(input);
-    string command, arg1, arg2, operand;
-    double a, b;
-    ss >> command >> operand >> arg1 >> arg2; // SOLVE (OPERAND ARG1 ARG2)
-    try
-    {
-        a = stod(arg1);
-        b = stod(arg2.erase(arg2.size() - 1)); // Erases closing bracket at ARG2
-    }
-    catch(const std::exception& e)
-    {
-        *err_code = TEXT_WRONG_FORMAT;
-        return 0;
-    }
-    
-    if(command == "SOLVE"){
+double text_solve(string input, short* err_code){
+    int result = 0;
+    size_t pos = input.find(" ");
+    string command = input.substr(0, pos);
+    string expr = input.substr(pos+1);
+    if(command == "SOLVE")
         *err_code = TEXT_OK;
-        switch(input[7]){
-            case '+':
-                return a + b;
-                break;
-            case '-':
-                return a - b;
-                break;
-            case '*':
-                return a * b;
-                break;
-            case '/':
-                return a / b;
-                break;
-            default:
-                *err_code = TEXT_WRONG_OPERAND;
-                return 0;
-        }
-    }else
+    else
         *err_code = TEXT_WRONG_COMMAND;
-    return 0;
+    bool ok_flag = true;
+    result = lisp(expr, &ok_flag);
+    if(!ok_flag)
+        *err_code = TEXT_WRONG_FORMAT;
+    return result;
+}
+
+// Function to compute lisp expression
+int expr(const smatch& match) {
+    char op = match[1].str()[0];
+    int a = stoi(match[2].str());
+    int b = stoi(match[3].str());
+    switch (op) {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return a / b;
+        default: 
+            print_err("Invalid lisp\n");
+            return -1;
+    }
+}
+
+// Function to parse lisp expression
+int lisp(string data, bool* ok) {
+    data = regex_replace(data, std::regex(R"(\n)"), "");
+    while (!regex_match(data, regex(R"(\d+)"))) {
+        smatch subexpr;
+        if (!regex_search(data, subexpr, LISP_RE)) {
+            *ok = false;
+            return 0;
+        }
+        int subres = expr(subexpr);
+        // data = regex_replace(data, LISP_RE, to_string(subres), "");
+        data = regex_replace(data, LISP_RE, to_string(subres), regex_constants::format_first_only);
+    }
+    *ok = true;
+    return stoi(data);
 }
 
 // Basic function to print ERRORs
@@ -241,4 +351,3 @@ void print_err(string msg){
     cerr << msg << endl;
     exit(EXIT_FAILURE);
 }
-
